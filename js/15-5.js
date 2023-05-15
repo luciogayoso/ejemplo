6c0edbb88c8d919796827f094cbb34c45955b852
luciogayoso <luciogayoso@gmail.com>
2023-05-15 16:16:00 -0300

    Update 15-5.js
    borrado

--- js/15-5.js
+++ js/15-5.js
@@ -1,6 +1,8 @@
 function ejemplo(params) {
-    let inp = document.querySelector("#entrada");
+    var inp = document.querySelector("#entrada");
     let valor = inp.value;
     console.log(valor);
     console.log(inp);
 }
+
+
