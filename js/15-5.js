ed79d5d18274686743ec25fbff87d44412c4a439
luciogayoso <luciogayoso@gmail.com>
2023-05-15 17:49:08 -0300

    Update 15-5.js

--- js/15-5.js
+++ js/15-5.js
@@ -1,8 +1,17 @@
+let num = 545
 function ejemplo(params) {
-    var inp = document.querySelector("#entrada");
+    let inp = document.querySelector("#entrada");
+    let p = document.querySelector("#salida");
+    let divImg = document.querySelector("#img");
+    divImg.innerHTML = '<img src="../img/ojo.jfif" alt="error"/>';
     let valor = inp.value;
+    p.innerText = valor;
+    console.log(p.value);
     console.log(valor);
-    console.log(inp);
+    console.dir(inp);
+    console.log(num);
+    let r = num - "45";
+    console.log(r);
 }
 
 
