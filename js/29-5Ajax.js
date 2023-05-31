a1660d2f73133eae835a9a3fee0708d5c66283ab
luciogayoso <luciogayoso@gmail.com>
2023-05-31 15:30:26 -0300

    Correccion Ejercicio 3

--- js/29-5Ajax.js
+++ js/29-5Ajax.js
@@ -1,4 +1,4 @@
-function Ejerccio1() {
+function Ejerc1cio1() {
     var meses = ["Enero","Febrero","Marzo","Abrir","Mayo","junio","julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 
     for (let i = 0; i < meses.length; i++) {
@@ -33,7 +33,7 @@ if (valores[3].length > valores[4].length) {
 
 
 function Ejercicio3() {
-    
+
     var numero1 = 5;
     var numero2 = 8;
 if(numero1 < numero2) {
@@ -45,6 +45,7 @@ console.log("numero2 es positivo");
 if(numero1 < 0 || numero1 != 0) {
 console.log("numero1 es negativo o distinto de cero");
 }
+numero1 = numero1 + 1;
 if(numero2 > numero1) {
 console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
 }
@@ -52,7 +53,14 @@ console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igua
 
 
 
+function Ejercicio4(params) {
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+'E', 'T'];
 
+var dni = document.querySelector("#entrada").value;
+
+
+}
 
 
 
