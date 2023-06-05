2fc0ae3dc58ce2f3f20ea03f6db2afce7cfe61ab
luciogayoso <luciogayoso@gmail.com>
2023-06-05 15:07:06 -0300

    Ejercicio 4

--- js/29-5Ajax.js
+++ js/29-5Ajax.js
@@ -1,4 +1,4 @@
-function Ejerc1cio1() {
+function Ejercicio1() {
     var meses = ["Enero","Febrero","Marzo","Abrir","Mayo","junio","julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 
     for (let i = 0; i < meses.length; i++) {
@@ -57,8 +57,20 @@ function Ejercicio4(params) {
     var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
 'E', 'T'];
 
-var dni = document.querySelector("#entrada").value;
+var dni = document.querySelector("#dni").value;
+var letraUsuario = document.querySelector("#letra").value;
+var letraUsuarioMayuscula = letraUsuario.toUpperCase();
 
+if (dni > 0 && dni < 99999999) {
+    var posicion = dni % 23;
+    if (letras[posicion] == letraUsuarioMayuscula) {
+        console.log("El número DNI y la letra son correctos");
+    }else {
+        console.log("La letra que ha indicado no es correcta");
+    }
+}else {
+    console.log("El número proporcionado no es válido");
+}
 
 }
 
