5e6f789e90601a8cab08e65d51eee55a1e4fa50b
luciogayoso <luciogayoso@gmail.com>
2023-06-05 16:53:15 -0300

    ejercicio 5

--- js/29-5Ajax.js
+++ js/29-5Ajax.js
@@ -75,10 +75,24 @@ if (dni > 0 && dni < 99999999) {
 }
 
 
+function Ejercicio5() {
+    var entero = document.querySelector("#entero").value;
+    var aux = 1;
+    for (let i = entero; i > 0; i--) {
+        aux = aux * i;
+    }
+    console.log(aux);
+}
 
 
-
-
+function Ejercicio6(params) {
+    var num = documen.querySelector("#parOImpar");
+    if (num % 2 == 0) {
+        console.log("El número es par");
+    } else {
+        console.log("El número es impar");
+    }
+}
 
 
 
