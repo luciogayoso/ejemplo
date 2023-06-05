f9182a8ce87fac9ceda962c63cdb791b84e625ca
luciogayoso <luciogayoso@gmail.com>
2023-06-05 17:26:53 -0300

    ejercicio 6 y 7

--- js/29-5Ajax.js
+++ js/29-5Ajax.js
@@ -95,7 +95,27 @@ function Ejercicio6(params) {
 }
 
 
+function Ejercicio7() {
+    var cadena = document.querySelector("#Cadena").value;
+    let minuscula = 0;
+    let mayuscula = 0;
+   
+    for (let i = 0; i < cadena.length; i++) {
+        if (cadena.charCodeAt(i) > 64 && cadena.charCodeAt(i) < 91) {
+            mayuscula++;
+        } else if (cadena.charCodeAt(i) > 96 && cadena.charCodeAt(i) < 122){
+            minuscula++;
+        }
+    }
 
+    if (mayuscula > 0 && minuscula > 0) {
+        console.log("Es una mezcla de ambas.")
+    }else if (mayuscula == 0 && minuscula > 0) {
+        console.log("Formado sólo por minúsculas.")
+    }else if (mayuscula > 0 && minuscula == 0) {
+        console.log("Formado sólo por mayusculas.")
+    }
+}
 
 
 function solicitudAJAX() {
